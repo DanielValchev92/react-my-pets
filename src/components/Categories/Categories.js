a7b16ac2fd6cabbbac50bf2c92f964322e07461c
DanielValchev92 <dvalchev92@gmail.com>
2021-09-19 16:54:34 +0300

    add categories and details components

--- src/components/Categories/Categories.js
+++ src/components/Categories/Categories.js
@@ -1,55 +1,57 @@
+import { Component } from 'react';
+
+import * as petsService from '../../services/petsService'
+
+import Pet from '../Pet/Pet'
 import CategoryNavigation from "./CategoryNavigation/CategoryNavigation";
 
-const Categories = ({
-    match,
-}) => {
 
-    return (
-        <section className="dashboard">
-            <h1>Dashboard</h1>
+class Categories extends Component {
 
-            <CategoryNavigation />
+    constructor(props) {
+        super(props);
 
-            <ul className="other-pets-list">
-                <li className="otherPet">
-                    <h3>Name: Gosho</h3>
-                    <p>Category: Cat</p>
-                    <p className="img"><img src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png" /></p>
-                    <p className="description">This is not my cat Gosho</p>
-                    <div className="pet-info">
-                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
-                        <a href="#"><button className="button">Details</button></a>
-                        <i className="fas fa-heart"></i> <span> 2</span>
-                    </div>
-                </li>
-                <li className="otherPet">
-                    <h3>Name: Gosho</h3>
-                    <p>Category: Cat</p>
-                    <p className="img"><img src="https://pics.clipartpng.com/Cat_PNG_Clip_Art-2580.png" /></p>
-                    <p className="description">This is not my cat Gosho</p>
-                    <div className="pet-info">
-                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
-                        <a href="#"><button className="button">Details</button></a>
-                        <i className="fas fa-heart"></i> <span> 2</span>
-                    </div>
+        this.state = {
+            pets: [],
+            currentCategory: 'all',
+        }
+    }
 
-                </li>
-                <li className="otherPet">
-                    <h3>Name: Kiro</h3>
-                    <p>Category: Dog</p>
-                    <p className="img"><img src="http://www.stickpng.com/assets/images/580b57fbd9996e24bc43bbde.png" />
-                    </p>
-                    <p className="description">This is my dog Kiro</p>
-                    <div className="pet-info">
-                        <a href="#"><button className="button"><i className="fas fa-heart"></i> Pet</button></a>
-                        <a href="#"><button className="button">Details</button></a>
-                        <i className="fas fa-heart"></i> <span> 4</span>
-                    </div>
-                </li>
-            </ul>
-        </section>
-    );
+    componentDidMount() {
+        petsService.getAll()
+            .then(res => this.setState({ pets: res }))
+    }
 
+    componentDidUpdate(prevProps) {
+        const category = this.props.match.params.category;
+
+        if (prevProps.match.params.category == category) {
+            return;
+        }
+
+        petsService.getAll(category)
+            .then(res => {
+
+                this.setState({ pets: res, currentCategory: category })
+            })
+    }
+
+    render() {
+        return (
+            <div className="dashboard">
+                <h1>Dashboard</h1>
+
+                <CategoryNavigation />
+
+                <ul className="other-pets-list">
+                    {this.state.pets.map(x =>
+                        <Pet key={x.id} {...x} />
+                    )}
+                </ul>
+            </div>
+        );
+
+    }
 }
 
 export default Categories;
